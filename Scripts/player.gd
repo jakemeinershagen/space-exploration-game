158f4193c17f1f6391c4072ea7ff11fb35ad5b10
jakemeinershagen <jakemeinershagen@gmail.com>
2023-11-21 17:52:10 -0600

    created a really scuffed ship

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const MOUSE_SENSITIVITY = 0.1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -14,8 +15,8 @@ func _ready():
 
 func _input(event):
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		rotate_y(deg_to_rad(-event.relative.x))
-		cam_pivot.rotate_x(deg_to_rad(-event.relative.y))
+		rotate_y(deg_to_rad(-event.relative.x * MOUSE_SENSITIVITY))
+		cam_pivot.rotate_x(deg_to_rad(-event.relative.y * MOUSE_SENSITIVITY))
 		cam_pivot.rotation.x = clamp(cam_pivot.rotation.x, deg_to_rad(-90), deg_to_rad(30))
 	
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -30,7 +31,7 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
